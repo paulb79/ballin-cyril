f64d22c91750a2e57ffc77798dfbadcc26ffbe7a
paulb79 <paulb@line.couk>
2013-05-02 22:23:43 +0100

    WIP project setup & config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BallinCyril::Application.config.secret_token = '9a77e2b90a3ec1a823980841cc15bfa00e1c160cbbd99eb28699bbd356d5ff6009d85e93763c0a5b991977221a9a77f832f518852aadd9565789e08023b78f83'
